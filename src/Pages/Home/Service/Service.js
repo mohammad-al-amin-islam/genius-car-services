5dd9094d001f748e44e309eff763f68596f0cb71
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-11 15:19:43 +0600

    services added

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Service.css'
+
+const Service = ({ service }) => {
+    const { name, img, price, description } = service;
+    return (
+        <div className='service'>
+
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>Price: {price}</p>
+            <p><small>{description}</small></p>
+            <button>Take Service: {name}</button>
+        </div>
+    );
+};
+
+export default Service;
