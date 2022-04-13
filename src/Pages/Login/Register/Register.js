103271ca8fbbda5f9960fca1bbb76be76a8ded58
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-13 11:32:15 +0600

    register form added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h3>This is register</h3>
+        <div className='register-from'>
+            <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Please Register</h3>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" placeholder='Enter Your Name' />
+                <input type="email" name="email" id="" placeholder='Enter your mail' required />
+                <input type="password" name="password" placeholder='Enter Your password' id="" required />
+                <input type="submit" value="Register" />
+            </form>
+            <p className='text-center'>Already have an account?<span onClick={() => navigate('/login')} className='text-primary' style={{ cursor: 'pointer' }}>Please Login</span></p>
         </div>
     );
 };
